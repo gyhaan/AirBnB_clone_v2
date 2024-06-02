19e995db78bd09168aa8d1c8510fdae5379fcd2e
Clinton Pikita <112552126+Clint07-datascientist@users.noreply.github.com>
2024-06-02 19:13:01 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,77 +1,79 @@
 #!/usr/bin/python3
-"""Test cases for BaseModel class"""
+""" """
+from models.base_model import BaseModel
 import unittest
 import datetime
+from uuid import UUID
+import json
 import os
-from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test cases for BaseModel class"""
+    """ Test base model"""
 
     def __init__(self, *args, **kwargs):
-        """Initialization"""
+        """ """
         super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
         self.value = BaseModel
 
+    def setUp(self):
+        """ """
+        pass
+
     def tearDown(self):
-        """Remove file.json after each test"""
         try:
             os.remove('file.json')
-        except FileNotFoundError:
+        except:
             pass
 
     def test_default(self):
-        """Test default instantiation"""
+        """ """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """Test instantiation with kwargs"""
+        """ """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """Test instantiation with kwargs containing int"""
+        """ """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
         with self.assertRaises(TypeError):
-            BaseModel(**copy)
+            new = BaseModel(**copy)
 
     def test_todict(self):
-        """Test to_dict method"""
+        """ """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """Test instantiation with kwargs containing None"""
+        """ """
         n = {None: None}
         with self.assertRaises(TypeError):
-            self.value(**n)
+            new = self.value(**n)
 
     def test_id(self):
-        """Test id attribute"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """Test created_at attribute"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """Test updated_at attribute"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertAlmostEqual(new.created_at.timestamp(),
                                new.updated_at.timestamp(), delta=1)
-
-
-if __name__ == '__main__':
-    unittest.main()
